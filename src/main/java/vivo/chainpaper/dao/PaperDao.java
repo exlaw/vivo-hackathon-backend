30323c997822d9968d2d8371d2d99da4e91a9f29
wangruihua <wangruihua88@yahoo.com>
2019-05-26 19:19:16 +0800

    add find writer method
    Signed-off-by: tony <wangruihua88@yahoo.com>

--- src/main/java/vivo/chainpaper/dao/PaperDao.java
+++ src/main/java/vivo/chainpaper/dao/PaperDao.java
@@ -1,8 +1,10 @@
 package vivo.chainpaper.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import vivo.chainpaper.entity.Paper;
 
 public interface PaperDao extends JpaRepository<Paper, String> {
-
+    @Query(value = "select p.writer from Paper p where p.id=?1")
+    String findUsernameByPaperId(String paperId);
 }
