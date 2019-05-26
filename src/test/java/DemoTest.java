2a7e4bb2dc6d409f9f8e31c0a5a4f112c3bc28bc
law <161250071@smail.nju.edu.cn>
2019-05-26 11:21:26 +0800

    jacoco

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -2,9 +2,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import vivo.chainpaper.MainApplication;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = MainApplication.class)
 public class DemoTest {
 
     @Test
